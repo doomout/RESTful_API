704968e18016e9baf7eec4a57aa4b25086b29dad
khg <winquake3@hanmail.net>
2026-05-22 21:36:40 +0900

    서비스 -> 컨트롤러에 생성자 주입

--- ex1/src/main/java/com/RESTfulAPI/khg/controller/SampleController.java
+++ ex1/src/main/java/com/RESTfulAPI/khg/controller/SampleController.java
@@ -3,12 +3,18 @@ package com.RESTfulAPI.khg.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.RESTfulAPI.khg.service.SampleService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/api/v1/sample")
 @Log4j2
+@RequiredArgsConstructor
 public class SampleController {
+    private final SampleService sampleService;
+
     @RequestMapping("/hello")
     public String[] hello() {
         return new String[]{"Hello, World!"};
