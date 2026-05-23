0a44bec5cee60afa110b327f23e8d09cc830b44e
khg <winquake3@hanmail.net>
2026-05-23 23:45:22 +0900

    Todo 기초 코드 추가

---
+++ Todo/khg/src/test/java/com/todo/khg/KhgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.khg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
