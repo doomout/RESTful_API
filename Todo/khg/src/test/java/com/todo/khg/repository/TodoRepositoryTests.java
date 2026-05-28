9f5b917d10b9e3606a1b1fc4d70954fb35b456a5
khg <winquake3@hanmail.net>
2026-05-28 20:58:53 +0900

    데이터 업데이트 테스트 코드

--- Todo/khg/src/test/java/com/todo/khg/repository/TodoRepositoryTests.java
+++ Todo/khg/src/test/java/com/todo/khg/repository/TodoRepositoryTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -54,5 +55,25 @@ public class TodoRepositoryTests {
         result.ifPresent(todoEntity -> {
             System.out.println(todoEntity);
         });
+    }
+
+    @Test
+    @Transactional // 트랜잭션을 적용하여 영속성 컨텍스트 유지
+    @Commit // 테스트 완료 후 트랜잭션 커밋하여 변경 사항 데이터베이스에 반영
+    public void testUpdateDirtyCheck() {
+        Long mno = 58L; // 수정할 데이터의 MNO
+        
+        // 동일 트랜젝션 내에서 처리되고 있는 영속 상태의 엔티티 객체
+        Optional<TodoEntity> result = todoRepository.findById(mno);
+
+        TodoEntity todoEntity = result.get();
+
+        System.out.println("OLD" + todoEntity);
+        
+        // 엔티티 객체의 상태 변경
+        todoEntity.changeTitle("수정된 제목..." + Math.random());
+        todoEntity.changeWriter("수정된 작성자..." + Math.random());
+
+        System.out.println("수정된 날짜: " + todoEntity);
     }
 }
