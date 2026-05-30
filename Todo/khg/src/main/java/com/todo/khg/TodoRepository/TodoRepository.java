1e980146042e12ca2933c997eb2f2227ecd34422
khg <winquake3@hanmail.net>
2026-05-30 22:29:32 +0900

    Query  로 페이징 처리를 위한 인터페이스 추가

--- Todo/khg/src/main/java/com/todo/khg/TodoRepository/TodoRepository.java
+++ Todo/khg/src/main/java/com/todo/khg/TodoRepository/TodoRepository.java
@@ -1,8 +1,11 @@
 package com.todo.khg.TodoRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import com.todo.khg.entity.TodoEntity;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-    
+    @Query("select t from TodoEntity t")
+    Page<TodoEntity> findAll(Pageable pageable);
 }
