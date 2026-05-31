e1833aa52a2c3a56a19b1aa5c48b74b398c85faa
khg <winquake3@hanmail.net>
2026-05-31 23:14:24 +0900

    TodoSearch interface 정의

--- Todo/khg/src/main/java/com/todo/khg/repository/search/TodoSearch.java
+++ Todo/khg/src/main/java/com/todo/khg/repository/search/TodoSearch.java
@@ -1,5 +1,9 @@
 package com.todo.khg.repository.search;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.todo.khg.entity.TodoEntity;
+
 public interface TodoSearch {
-    
+    Page<TodoEntity> search1(Pageable pageable);
 }
