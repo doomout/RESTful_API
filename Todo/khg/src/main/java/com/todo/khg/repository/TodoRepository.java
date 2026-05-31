5bf0ad5e77d74606458c399c9f942c47060b7599
khg <winquake3@hanmail.net>
2026-05-31 23:52:55 +0900

    todoSearch 인터페이스 추가

--- Todo/khg/src/main/java/com/todo/khg/repository/TodoRepository.java
+++ Todo/khg/src/main/java/com/todo/khg/repository/TodoRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.todo.khg.entity.TodoEntity;
+import com.todo.khg.repository.search.TodoSearch;
 
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+public interface TodoRepository extends JpaRepository<TodoEntity, Long>, TodoSearch {
     @Query("select t from TodoEntity t ")
     Page<TodoEntity> listAll(Pageable pageable);
 }
