41fd856fd079eb89af7c582efde896141bd2aed6
khg <winquake3@hanmail.net>
2026-06-02 21:57:16 +0900

    DTO로 변환하여 반환하는 메서드 추가

--- Todo/khg/src/main/java/com/todo/khg/repository/search/TodoSearch.java
+++ Todo/khg/src/main/java/com/todo/khg/repository/search/TodoSearch.java
@@ -2,8 +2,13 @@ package com.todo.khg.repository.search;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import com.todo.khg.dto.TodoDTO;
 import com.todo.khg.entity.TodoEntity;
 
 public interface TodoSearch {
     Page<TodoEntity> search1(Pageable pageable);
+
+    // DTO로 변환하여 반환하는 메서드 추가
+    Page<TodoDTO> searchDTO(Pageable pageable);
 }
