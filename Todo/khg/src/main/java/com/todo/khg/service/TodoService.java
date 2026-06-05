54c0a24387699e670a9b0a8516ec5afebb0aaf9d
khg <winquake3@hanmail.net>
2026-06-05 22:46:14 +0900

    register 메소드 선언

--- Todo/khg/src/main/java/com/todo/khg/service/TodoService.java
+++ Todo/khg/src/main/java/com/todo/khg/service/TodoService.java
@@ -4,6 +4,7 @@ package com.todo.khg.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.todo.khg.dto.TodoDTO;
 import com.todo.khg.repository.TodoRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,4 +16,12 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepository;
+
+     public TodoDTO register(TodoDTO todoDTO) {
+        // DTO 를 엔티티 객체로 변환
+        // TodoRepository 를 이용하여 저장
+        // DTO에 저장된 번호를 지정해서 반환
+        
+        return null;
+     }
 }
