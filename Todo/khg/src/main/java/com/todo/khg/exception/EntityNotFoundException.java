10a50dd067a5bf274e38ddb6650a27e6ad2f895e
khg <winquake3@hanmail.net>
2026-06-06 22:09:03 +0900

    404 예외 처리 코드 추가

---
+++ Todo/khg/src/main/java/com/todo/khg/exception/EntityNotFoundException.java
@@ -0,0 +1,17 @@
+package com.todo.khg.exception;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class EntityNotFoundException extends RuntimeException {
+    private String message;
+    private int code;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+        this.message = message;
+        this.code = 404; // HTTP 404 Not Found
+    }
+}
