e422f8c5ae94940cbc0ee730e8674f7d4c28b02c
khg <winquake3@hanmail.net>
2026-06-08 22:51:36 +0900

    검증 관련 어노테이션 추가

--- Todo/khg/src/main/java/com/todo/khg/dto/TodoDTO.java
+++ Todo/khg/src/main/java/com/todo/khg/dto/TodoDTO.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import com.todo.khg.entity.TodoEntity;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TodoDTO {
     private Long mno;
+
+    @NotEmpty(message = "제목은 필수입니다.")
     private String title;
+
+    @NotEmpty(message = "작성자는 필수입니다.")
     private String writer;
+
+    @FutureOrPresent(message = "마감일은 오늘 또는 미래여야 합니다.")
     private LocalDate dueDate;
 
     // 엔티티 객체 -> DTO 객체로 변환하는 생성자
