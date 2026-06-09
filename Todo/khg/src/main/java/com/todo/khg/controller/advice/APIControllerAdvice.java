f26f9599c9f977489819830b2c722b900e8aaac7
khg <winquake3@hanmail.net>
2026-06-09 22:29:25 +0900

    타입이 일치 하지 않을 때 예외 처리

--- Todo/khg/src/main/java/com/todo/khg/controller/advice/APIControllerAdvice.java
+++ Todo/khg/src/main/java/com/todo/khg/controller/advice/APIControllerAdvice.java
@@ -8,13 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import com.todo.khg.exception.EntityNotFoundException;
 
 import lombok.extern.log4j.Log4j2;
 
 @RestControllerAdvice
 @Log4j2
 public class APIControllerAdvice { // API 컨트롤러에서 발생하는 예외를 처리하는 클래스
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class) // 매개변수의 타입이 일치하지 않을 때 발생하는 예외를 처리하는 메서드
+    public ResponseEntity<?> handleArgsException(MethodArgumentTypeMismatchException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("error", "Type Mismatch");
+        errors.put(exception.getName(), exception.getValue() + " is not valid value");
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // 매개변수의 유효성 검사가 실패할 때 발생하는 예외를 처리하는 메서드
     public ResponseEntity<?> handleArgsException(MethodArgumentNotValidException exception) {
         Map<String, Object> errors = new HashMap<>();
 
