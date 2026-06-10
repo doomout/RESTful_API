4f094ab12665bbeedcddace3109efaaa7db43154
khg <winquake3@hanmail.net>
2026-06-10 18:00:45 +0900

    목록 처리 기능 추가

--- Todo/khg/src/main/java/com/todo/khg/controller/TodoController.java
+++ Todo/khg/src/main/java/com/todo/khg/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.todo.khg.controller;
 
 import java.util.Map;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todo.khg.dto.PageRequestDTO;
 import com.todo.khg.dto.TodoDTO;
 import com.todo.khg.service.TodoService;
 
@@ -72,5 +74,19 @@ public class TodoController {
         Map<String, String> result = Map.of("result", "success");
 
         return ResponseEntity.ok(result); // 삭제 성공 시 204 No Content 응답 반환
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<TodoDTO>> list(@Validated PageRequestDTO pageRequestDTO) { // @Validated 어노테이션을 사용하여 유효성 검사를 수행
+        log.info("list...............");
+        log.info(pageRequestDTO);
+        // 페이지 요청 정보는 PageRequestDTO 객체로 받는다.
+        // 페이지 요청 정보는 URL 쿼리 파라미터로 전달된다. 예: /api/v1/todos/list?page=1&size=10
+        // @RequestParam 어노테이션을 사용하여 URL 쿼리 파라미터에서 page와 size 값을 추출한다.
+        // @Validated 어노테이션을 사용하여 유효성 검사를 수행한다.
+        // PageRequestDTO 객체는 page와 size 필드를 가지고 있으며, 기본값은 각각 1과 10이다.
+        // 페이지 요청 정보는 Pageable 객체로 변환되어 서비스 계층에 전달된다.
+
+        return ResponseEntity.ok(todoService.getList(pageRequestDTO)); // 페이지 목록과 페이지 정보를 포함하는 JSON 결과를 반환
     }
 }
