cf74f7f6b4bae65b4841678115323321c2207262
khg <winquake3@hanmail.net>
2026-06-12 02:57:35 +0900

    회원 관련 몇 가지 예외를 enum 타입으로 지정

---
+++ Todo/khg/src/main/java/com/todo/khg/member/exception/MemberExceptions.java
@@ -0,0 +1,17 @@
+package com.todo.khg.member.exception;
+
+public enum MemberExceptions {
+    NOT_FOUND("존재하지 않는 회원입니다.", 404),
+    DUPLICATE("이미 존재하는 회원입니다.", 409),
+    INVALID("유효하지 않은 회원 정보입니다.", 400);
+
+    private MemberTaskExceptiopn memberTaskExceptiopn;
+
+    MemberExceptions(String msg, int code) {
+        memberTaskExceptiopn = new MemberTaskExceptiopn(msg, code);
+    }
+
+    public MemberTaskExceptiopn get() {
+        return memberTaskExceptiopn;
+    }
+}
