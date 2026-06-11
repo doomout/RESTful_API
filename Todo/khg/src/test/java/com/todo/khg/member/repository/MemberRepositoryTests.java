1a39d55b48aab6b86bafce44878f9878199f27d6
khg <winquake3@hanmail.net>
2026-06-12 02:39:39 +0900

    등록 테스트 코드

--- Todo/khg/src/test/java/com/todo/khg/member/repository/MemberRepositoryTests.java
+++ Todo/khg/src/test/java/com/todo/khg/member/repository/MemberRepositoryTests.java
@@ -1,11 +1,33 @@
 package com.todo.khg.member.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.todo.khg.entity.MemberEntity;
+import com.todo.khg.repository.MemberRepository;
+
 @SpringBootTest
 public class MemberRepositoryTests {
     @Autowired
+    private MemberRepository memberRepository;
+    
+    @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testInsert() {
+        for (int i = 1; i <= 100; i++) {
+            MemberEntity memberEntity = MemberEntity.builder()
+                    .mid("user" + i)
+                    .mpw(passwordEncoder.encode("1111"))
+                    .mname("USER" + i)
+                    .email("user" + i + "@aaa.com")
+                    .role(i <= 80 ? "USER" : "ADMIN") // user1 ~ user80 : USER, user81 ~ user100 : ADMIN
+                    .build();
+
+            memberRepository.save(memberEntity);
+        } 
+    }
 }
