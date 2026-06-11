7ae356c451b5a1e875146e17dedc433c49047623
khg <winquake3@hanmail.net>
2026-06-12 03:01:42 +0900

    아이디가 없는 경우 테스트

--- Todo/khg/src/test/java/com/todo/khg/member/repository/MemberRepositoryTests.java
+++ Todo/khg/src/test/java/com/todo/khg/member/repository/MemberRepositoryTests.java
@@ -1,11 +1,14 @@
 package com.todo.khg.member.repository;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.todo.khg.entity.MemberEntity;
+import com.todo.khg.member.exception.MemberExceptions;
 import com.todo.khg.repository.MemberRepository;
 
 @SpringBootTest
@@ -29,5 +32,16 @@ public class MemberRepositoryTests {
 
             memberRepository.save(memberEntity);
         } 
+    }
+
+    @Test
+    public void testRead() { // 아이디가 없는 경우 테스트
+        String mid = "user1";
+
+        Optional<MemberEntity> result = memberRepository.findById(mid);
+
+        MemberEntity memberEntity = result.orElseThrow(MemberExceptions.NOT_FOUND::get);
+
+        System.out.println(memberEntity);
     }
 }
