7245cdfda78256c7577c9f0f5a0e0d53fdb45361
khg <winquake3@hanmail.net>
2026-06-14 08:21:48 +0900

    SecurityFilterChain 기초 코드 추가

--- ex3/src/main/java/com/ex3/khg/config/CustomSecurityConfig.java
+++ ex3/src/main/java/com/ex3/khg/config/CustomSecurityConfig.java
@@ -2,14 +2,23 @@ package com.ex3.khg.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.extern.log4j.Log4j2;
 
 @Configuration
 @Log4j2
 public class CustomSecurityConfig {
+    @Bean 
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        log.info("filter chain......");
+
+        return httpSecurity.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
