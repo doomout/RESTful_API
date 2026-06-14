e366eb80d294faa483bc8180902c4cda980a9463
khg <winquake3@hanmail.net>
2026-06-14 12:25:23 +0900

    토큰 발행시 일어날수 있는 예외 처리 문구 추가

--- ex3/src/main/java/com/ex3/khg/member/exception/MemberExceptions.java
+++ ex3/src/main/java/com/ex3/khg/member/exception/MemberExceptions.java
@@ -4,7 +4,8 @@ package com.ex3.khg.member.exception;
 public enum MemberExceptions {
     NOT_FOUND("존재하지 않는 회원입니다.", 404),
     DUPLICATE("이미 존재하는 회원입니다.", 409),
-    INVALID("유효하지 않은 회원 정보입니다.", 400);
+    INVALID("유효하지 않은 회원 정보입니다.", 400),
+    BAD_CREDENTIALS("잘못된 자격 증명입니다.", 401);
 
     private MemberTaskException MemberTaskException;
 
