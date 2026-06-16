176842eb3765680c4821336eebbab122b4929993
khg <winquake3@hanmail.net>
2026-06-16 23:29:56 +0900

    관리자 권한 없으면 막음

--- ex3/src/main/java/com/ex3/khg/sample/controller/SampleController.java
+++ ex3/src/main/java/com/ex3/khg/sample/controller/SampleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j2
 @RequestMapping("/api/v1/samples")
 public class SampleController { // 접근 제한 테스트를 위한 컨트롤러
+    @PreAuthorize("hasRole('ROLE_ADMIN')") // 관리자 권한 없으면 막음
     @GetMapping("/list")
     public ResponseEntity<?> list() {
         log.info("list............");
