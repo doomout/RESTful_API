002d8574b7e7f49ac0b5b672936b0291efd32588
khg <winquake3@hanmail.net>
2026-06-16 22:46:38 +0900

    메서드 단위로 권한 검사하는 기능 추가

--- ex3/src/main/java/com/ex3/khg/config/CustomSecurityConfig.java
+++ ex3/src/main/java/com/ex3/khg/config/CustomSecurityConfig.java
@@ -2,6 +2,7 @@ package com.ex3.khg.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Configuration
 @Log4j2
+@EnableMethodSecurity(prePostEnabled = true) // 메서드 단위로 권한 검사하는 기능을 켜겠다
 public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
