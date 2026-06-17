71bb8d53875bd4da8823cbc7a6c56952170b6e31
khg <winquake3@hanmail.net>
2026-06-17 23:52:11 +0900

    접근 권한없는 사용자 예외 처리 추가

--- ex3/src/main/java/com/ex3/khg/member/controller/advice/TokenControllerAdvice.java
+++ ex3/src/main/java/com/ex3/khg/member/controller/advice/TokenControllerAdvice.java
@@ -1,8 +1,11 @@
 package com.ex3.khg.member.controller.advice;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +25,15 @@ public class TokenControllerAdvice {
         Map<String, String> map = Map.of("error", msg);
 
         return ResponseEntity.status(status).body(map);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException exception) {
+        log.info("handleAccessDeniedExcption...............");
+
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("message", exception.getMessage());
+
+        return new ResponseEntity<>(errors, HttpStatus.FORBIDDEN);
     }
 }
