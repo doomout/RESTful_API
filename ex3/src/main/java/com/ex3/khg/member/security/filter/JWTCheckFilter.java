6fd18f3b21c9a40acf9a1b646aaf2eb281f721cc
khg <winquake3@hanmail.net>
2026-06-18 22:26:41 +0900

    JWTCheckFilter 에 기능 추가

--- ex3/src/main/java/com/ex3/khg/member/security/filter/JWTCheckFilter.java
+++ ex3/src/main/java/com/ex3/khg/member/security/filter/JWTCheckFilter.java
@@ -2,22 +2,50 @@ package com.ex3.khg.member.security.filter;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.ex3.khg.member.security.util.JWTUtil;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Component
+@RequiredArgsConstructor
+@Log4j2
 public class JWTCheckFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+    
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return super.shouldNotFilter(request);
+        // 경로 지정 필요
+        return false;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
         throws ServletException, IOException {
-        
+        log.info("JWTCheckFilter doFilter............");
+        log.info("requestURL" + request.getRequestURI());
+        String headerStr = request.getHeader("Authorization"); 
+        log.info("headerStr: " + headerStr);
+
+        // Access Token이 없는 경우
+        if(headerStr == null || !headerStr.startsWith("Bearer ")) {
+            handleException(response, new Exception("ACCESS ToKEN NOT FOUND"));
+
+            return;
+        }
+    }
+
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.getWriter().println("{\"error\": \"" + e.getMessage() + "\"}");
     }
 }
+ 
