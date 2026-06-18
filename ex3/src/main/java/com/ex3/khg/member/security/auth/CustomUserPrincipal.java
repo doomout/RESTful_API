71a09041a1eec440a724fe34844131c42c271128
khg <winquake3@hanmail.net>
2026-06-19 03:43:59 +0900

    Principal 인터페이스 구현

---
+++ ex3/src/main/java/com/ex3/khg/member/security/auth/CustomUserPrincipal.java
@@ -0,0 +1,15 @@
+package com.ex3.khg.member.security.auth;
+
+import java.security.Principal;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CustomUserPrincipal implements Principal {
+    private final String mid;
+
+    @Override
+    public String getName() {
+        return mid;
+    }
+}
