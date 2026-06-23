04c9890a25e1a3afabaf6c86b7a048fbfb76d7b6
khg <winquake3@hanmail.net>
2026-06-24 08:30:29 +0900

    토큰 생성 부분을 호출하는 함수 추가

--- front/js/api.js
+++ front/js/api.js
@@ -1,3 +1,14 @@
+import axios from 'axios'
+
 export const testApi = () => {
     console.log("test Api.....")
+}
+const url = "http://localhost:8080/api/v1/"
+
+export const makeToken = (mid, mpw) => {
+    const path = url + "token/make"
+
+    const data = {mid, mpw}
+    
+    axios.post(path, data)
 }
