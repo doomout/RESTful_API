6eb719097e9a10453f3dec974c2a6cda5f419107
khg <winquake3@hanmail.net>
2026-06-25 07:53:32 +0900

    쿠키 기능 설정(기본 경로 /, 30일 동안 저장)

--- front/js/api.js
+++ front/js/api.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
+import Cookies from "universal-cookie";
 
 export const testApi = () => {
     console.log("test Api.....")
@@ -8,10 +9,15 @@ const url = "http://localhost:8080/api/v1/"
 // 비동기 처리를 위한 async/await 추가
 export const makeToken = async (mid, mpw) => {
     const path = url + "token/make"
-
     const data = {mid, mpw}
-
     const res = await axios.post(path, data)
+
+    // 쿠키 기능 설정(기본 경로 /, 30일 동안 저장)
+    const cookies = new Cookies(null, {path: '/', maxAge: 2592000})
+    
+    export const saveToken = (tokenName, tokenValue) => {
+        cookies.set(tokenName, tokenValue)
+    }
     
     // 결과 데이터 반환
     return res.data
