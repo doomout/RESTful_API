49376f9be9f020794491196e17785b034f4b3642
khg <winquake3@hanmail.net>
2026-07-01 20:20:09 +0900

    이미지 파일 확장자 체크 예외 기초 코드

---
+++ ex3/src/main/java/com/ex3/khg/upload/exception/UploadNotSupportedExcepiton.java
@@ -0,0 +1,8 @@
+package com.ex3.khg.upload.exception;
+
+public class UploadNotSupportedExcepiton extends RuntimeException {
+    public UploadNotSupportedExcepiton(String message) {
+        super(message);
+    }
+    
+}
