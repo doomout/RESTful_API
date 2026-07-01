bcdab2e555c22f96194ab679c61c455b874d0e7e
khg <winquake3@hanmail.net>
2026-07-01 20:24:27 +0900

    업로드 파일 확장자 체크하는 기능 추가

--- ex3/src/main/java/com/ex3/khg/upload/controller/advice/FileControllerAdvice.java
+++ ex3/src/main/java/com/ex3/khg/upload/controller/advice/FileControllerAdvice.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+import com.ex3.khg.upload.exception.UploadNotSupportedExcepiton;
+
 @RestControllerAdvice
 public class FileControllerAdvice {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
@@ -14,4 +16,8 @@ public class FileControllerAdvice {
         return ResponseEntity.badRequest().body(Map.of("error","File too large"));
     }
 
+    @ExceptionHandler(UploadNotSupportedExcepiton.class)
+    public ResponseEntity<Map<String, String>> handleUploadNotSupportedException(UploadNotSupportedExcepiton exception) {
+        return ResponseEntity.badRequest().body(Map.of("error", exception.getMessage()));
+    }
 }
