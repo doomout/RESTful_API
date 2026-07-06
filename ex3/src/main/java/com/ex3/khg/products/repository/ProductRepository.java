62d00c0c3fd369128d37db4d8ef7fe50848fb78d
khg <winquake3@hanmail.net>
2026-07-06 22:50:05 +0900

    이미지는 측시 로딩으로 가져오기 위해 추가

--- ex3/src/main/java/com/ex3/khg/products/repository/ProductRepository.java
+++ ex3/src/main/java/com/ex3/khg/products/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.ex3.khg.products.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ex3.khg.products.entity.ProductEntity;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    
+    // images 속성을 즉시 로딩으로 가져오기 위해 EntityGraph를 사용
+    @EntityGraph(attributePaths = "images", type = EntityGraph.EntityGraphType.FETCH)
+    @Query("SELECT p FROM ProductEntity p WHERE p.pno = :pno")
+    Optional<ProductEntity> findByIdWithImages(@Param("pno") Long pno); 
 }
