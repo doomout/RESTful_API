f61c0415921f8d2f22358757d60609f305d9382e
khg <winquake3@hanmail.net>
2026-07-09 05:26:54 +0900

    ProductDTO를 반환하도록 JPQL 쿼리 작성

--- ex3/src/main/java/com/ex3/khg/products/repository/ProductRepository.java
+++ ex3/src/main/java/com/ex3/khg/products/repository/ProductRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.ex3.khg.products.dto.ProductDTO;
 import com.ex3.khg.products.entity.ProductEntity;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
@@ -16,6 +17,10 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     // Optional<ProductEntity> getProduct(@Param("pno") Long pno); 
 
     // Fetch Join(JPA 에서만 사용)로 images 속성을 즉시 로딩으로 가져오기
+    // @Query("SELECT p FROM ProductEntity p JOIN FETCH p.images pi WHERE p.pno = :pno")
+    // Optional<ProductEntity> getProduct(@Param("pno") Long pno);
+
+    // ProductDTO를 반환하도록 JPQL 쿼리 작성
     @Query("SELECT p FROM ProductEntity p JOIN FETCH p.images pi WHERE p.pno = :pno")
-    Optional<ProductEntity> getProduct(@Param("pno") Long pno);
+    Optional<ProductDTO> getProductDTO(@Param("pno") Long pno);
 }
