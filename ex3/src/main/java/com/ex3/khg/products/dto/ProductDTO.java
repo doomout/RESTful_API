b1842ee2e0f2cd7c8eab034f97daa19b8628ec75
khg <winquake3@hanmail.net>
2026-07-08 23:25:04 +0900

    상품 등록 DTO 정의

---
+++ ex3/src/main/java/com/ex3/khg/products/dto/ProductDTO.java
@@ -0,0 +1,23 @@
+package com.ex3.khg.products.dto;
+
+import java.util.List;
+
+import com.ex3.khg.products.entity.ProductEntity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ProductDTO {
+    private Long pno;
+    private String pname;
+    private int price;
+    private String content;
+    private String writer;
+
+    private List[] imageList; // 이미지 파일 이름 배열
+    public ProductDTO(ProductEntity productEntity) {
+        
+    }
+}
