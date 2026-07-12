e5f9cee7472195319ed2965050f298f1d4a14ec5
khg <winquake3@hanmail.net>
2026-07-13 06:39:47 +0900

    listWithAllImages 인터페이스 추가

--- ex3/src/main/java/com/ex3/khg/products/repository/search/ProductSearch.java
+++ ex3/src/main/java/com/ex3/khg/products/repository/search/ProductSearch.java
@@ -3,8 +3,10 @@ package com.ex3.khg.products.repository.search;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.ex3.khg.products.dto.ProductDTO;
 import com.ex3.khg.products.dto.ProductListDTO;
 
 public interface ProductSearch  {
     Page<ProductListDTO> list(Pageable pageable);
+    Page<ProductDTO>  listWithAllImages(Pageable pageable);
 }
