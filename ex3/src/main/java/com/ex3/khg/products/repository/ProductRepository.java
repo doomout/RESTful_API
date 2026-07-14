3da3d56fdb0c18b54c1d240c304e6d9222f74250
khg <winquake3@hanmail.net>
2026-07-14 23:45:30 +0900

    Fetch Join로 images 속성을 즉시 로딩으로 가져오기

--- ex3/src/main/java/com/ex3/khg/products/repository/ProductRepository.java
+++ ex3/src/main/java/com/ex3/khg/products/repository/ProductRepository.java
@@ -20,8 +20,8 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long>, P
     // Optional<ProductEntity> getProduct(@Param("pno") Long pno); 
 
     // Fetch Join(JPA 에서만 사용)로 images 속성을 즉시 로딩으로 가져오기
-    // @Query("SELECT p FROM ProductEntity p JOIN FETCH p.images pi WHERE p.pno = :pno")
-    // Optional<ProductEntity> getProduct(@Param("pno") Long pno);
+    @Query("SELECT p FROM ProductEntity p JOIN FETCH p.images pi WHERE p.pno = :pno")
+    Optional<ProductEntity> getProduct(@Param("pno") Long pno);
 
     // ProductDTO를 반환하도록 JPQL 쿼리 작성
     @Query("SELECT p FROM ProductEntity p JOIN FETCH p.images pi WHERE p.pno = :pno")
