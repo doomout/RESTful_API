d33002ed792467baafd8779d410ee220d4a54d6e
khg <winquake3@hanmail.net>
2026-07-17 08:37:51 +0900

    등록된 상품을 모두 반환하는 메서드

--- ex3/src/main/java/com/ex3/khg/products/service/ProductService.java
+++ ex3/src/main/java/com/ex3/khg/products/service/ProductService.java
@@ -2,6 +2,9 @@ package com.ex3.khg.products.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ex3.khg.products.dto.ProductDTO;
+import com.ex3.khg.products.entity.ProductEntity;
+import com.ex3.khg.products.exception.ProductException;
 import com.ex3.khg.products.repository.ProductRepository;
 
 import jakarta.transaction.Transactional;
@@ -14,4 +17,21 @@ import lombok.extern.log4j.Log4j2;
 @Transactional
 public class ProductService {
     private final ProductRepository productRepository;
+
+    // 등록된 상품을 모두 반환하도록 ProductDTO을 리턴 타입으로 사용
+    public ProductDTO register(ProductDTO productDTO) {
+        try {
+            log.info("register.............");
+            log.info(productDTO);
+
+            ProductEntity productEntity = productDTO.toEntity();
+
+            productRepository.save(productEntity);
+
+            return new ProductDTO(productEntity);
+        } catch(Exception e) {
+            log.error(e.getMessage());
+            throw ProductException.PRODUCT_NOT_REGISTERED.get();
+        }
+    }
 }
