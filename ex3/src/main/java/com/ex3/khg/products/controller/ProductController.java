4f9377df8b2c088aa4d36b491e22bff14049b5b0
khg <winquake3@hanmail.net>
2026-07-18 14:33:14 +0900

    사용자 정보를 확인하는 메서드 추가

--- ex3/src/main/java/com/ex3/khg/products/controller/ProductController.java
+++ ex3/src/main/java/com/ex3/khg/products/controller/ProductController.java
@@ -3,10 +3,20 @@ package com.ex3.khg.products.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ex3.khg.products.dto.PageRequestDTO;
+import com.ex3.khg.products.dto.ProductListDTO;
 import com.ex3.khg.products.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import java.security.Principal;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @Log4j2
@@ -14,4 +24,13 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/api/v1/products")
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<ProductListDTO>> list(@Validated PageRequestDTO pageRequestDTO, Principal principal) {
+        log.info(pageRequestDTO);
+        log.info(principal.getName());
+
+        return ResponseEntity.ok(productService.getList(pageRequestDTO));
+    }
+    
 }
