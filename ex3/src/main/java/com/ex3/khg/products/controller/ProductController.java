f6fd444e830189f46a1d267faec6d94bcb955e4d
khg <winquake3@hanmail.net>
2026-07-19 19:08:01 +0900

    컨트롤러에 예외 처리 추가

--- ex3/src/main/java/com/ex3/khg/products/controller/ProductController.java
+++ ex3/src/main/java/com/ex3/khg/products/controller/ProductController.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ex3.khg.products.dto.PageRequestDTO;
+import com.ex3.khg.products.dto.ProductDTO;
 import com.ex3.khg.products.dto.ProductListDTO;
+import com.ex3.khg.products.exception.ProductException;
 import com.ex3.khg.products.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -31,6 +36,24 @@ public class ProductController {
         log.info(principal.getName());
 
         return ResponseEntity.ok(productService.getList(pageRequestDTO));
+    }
+    
+    @PostMapping("")
+    public ResponseEntity<ProductDTO> register(@RequestBody @Validated ProductDTO productDTO, Principal principal) {
+        log.info("register.............");
+        log.info(productDTO);
+        
+        // 이미지 파일이 없는 경우
+        if(productDTO.getImageList() == null || productDTO.getImageList().isEmpty()) {
+            throw ProductException.PRODUCT_NO_IMAGE.get();
+        }
+
+        // 쓴 사람과 현재 사용자가 다를 때
+        if(!principal.getName().equals(productDTO.getWriter())) {
+            throw ProductException.PRODUCT_WRITER_ERROR.get();
+        }
+        
+        return ResponseEntity.ok(productService.register(productDTO));
     }
     
 }
