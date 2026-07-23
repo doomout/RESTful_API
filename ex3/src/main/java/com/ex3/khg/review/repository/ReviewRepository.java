551243410cdf580d63d05cf2f9cd52f4abd20350
khg <winquake3@hanmail.net>
2026-07-24 02:52:18 +0900

    리뷰, 상품 정보를 한번에 부르기 위해 fetch join 구현

--- ex3/src/main/java/com/ex3/khg/review/repository/ReviewRepository.java
+++ ex3/src/main/java/com/ex3/khg/review/repository/ReviewRepository.java
@@ -1,9 +1,16 @@
 package com.ex3.khg.review.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ex3.khg.review.entity.ReviewEntity;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long>{
-    
+    // 리뷰와 상품 정보를 같이 불러올 때 지연 로딩은 여러번 select가 발생하니 
+    // fetch join 으로 한번에 불러오기
+    @Query("select r from ReviewEntity r join fetch r.productEntity where r.rno = :rno")
+    Optional<ReviewEntity> getWithProdcut(@Param("rno") Long rno);
 }
