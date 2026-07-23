27bcde42af5d86a33f529f7e5d6b21bfe50af842
khg <winquake3@hanmail.net>
2026-07-24 03:11:43 +0900

    조회시 리뷰가 없는 경우 예외 추가

--- ex3/src/main/java/com/ex3/khg/review/exception/ReviewException.java
+++ ex3/src/main/java/com/ex3/khg/review/exception/ReviewException.java
@@ -2,7 +2,8 @@ package com.ex3.khg.review.exception;
 
 public enum ReviewException {
     REVIEW_NOT_REGISTERED("Review Not Registere", 400),
-    REVIEW_PRODUCT_NOT_FOUND("Product Not Found for Review", 404);
+    REVIEW_PRODUCT_NOT_FOUND("Product Not Found for Review", 404),
+    REVIEW_NOT_FOUND("Revidw Not Found", 404);
 
     private final ReviewTaskException reviewTaskException;
 
