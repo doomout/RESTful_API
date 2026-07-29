4f11a7371d1aba2ea139152a3837f8324d526bba
khg <winquake3@hanmail.net>
2026-07-29 20:04:53 +0900

    등록 테스트

--- ex3/src/test/java/com/ex3/khg/review/service/ReviewServiceTests.java
+++ ex3/src/test/java/com/ex3/khg/review/service/ReviewServiceTests.java
@@ -1,10 +1,27 @@
 package com.ex3.khg.review.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ex3.khg.review.dto.ReviewDTO;
 
 @SpringBootTest
 public class ReviewServiceTests {
     @Autowired
     private ReviewService reviewService;
+
+    // 등록 테스트
+    @Test
+    public void testRegister() {
+        Long pno = 999L; //존재 하지 않는 상품 번호
+        ReviewDTO reviewDTO = ReviewDTO.builder()
+                    .reviewText("리뷰내용")
+                    .score(5)
+                    .reviewer("악플러")
+                    .pno(pno)
+                    .build();
+
+        reviewService.register(reviewDTO);
+    }
 }
