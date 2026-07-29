d35ea6829a1f67c949a2f142e32a77265ead3b30
khg <winquake3@hanmail.net>
2026-07-29 20:17:07 +0900

    리뷰 조회 테스트

--- ex3/src/test/java/com/ex3/khg/review/service/ReviewServiceTests.java
+++ ex3/src/test/java/com/ex3/khg/review/service/ReviewServiceTests.java
@@ -16,12 +16,20 @@ public class ReviewServiceTests {
     public void testRegister() {
         Long pno = 999L; //존재 하지 않는 상품 번호
         ReviewDTO reviewDTO = ReviewDTO.builder()
-                    .reviewText("리뷰내용")
+                    .reviewText("이게 등록되면 안된다.")
                     .score(5)
-                    .reviewer("악플러")
+                    .reviewer("알겠냐")
                     .pno(pno)
                     .build();
 
-        reviewService.register(reviewDTO);
+        reviewService.register(reviewDTO); // 404 에러 나는게 정상
+    }
+
+    // 리뷰 조회 테스트
+    @Test
+    public void testRead(){
+        Long rno = 1L; // 존재하는 리뷰 번호
+        ReviewDTO reviewDTO = reviewService.read(rno);
+        System.out.println(reviewDTO);
     }
 }
