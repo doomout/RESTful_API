b265b27b4a5005858e454b9608754c9bea2f586b
khg <winquake3@hanmail.net>
2026-07-29 20:00:13 +0900

    서비스 테스트 기초 코드

---
+++ ex3/src/test/java/com/ex3/khg/review/service/ReviewServiceTests.java
@@ -0,0 +1,10 @@
+package com.ex3.khg.review.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReviewServiceTests {
+    @Autowired
+    private ReviewService reviewService;
+}
