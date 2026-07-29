c4868c4d375c4e64bbafe752696119049782c49c
khg <winquake3@hanmail.net>
2026-07-29 19:57:03 +0900

    등록처리 구현

--- ex3/src/main/java/com/ex3/khg/review/service/ReviewService.java
+++ ex3/src/main/java/com/ex3/khg/review/service/ReviewService.java
@@ -1,8 +1,12 @@
 package com.ex3.khg.review.service;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ex3.khg.review.dto.ReviewDTO;
+import com.ex3.khg.review.entity.ReviewEntity;
+import com.ex3.khg.review.exception.ReviewException;
 import com.ex3.khg.review.repository.ReviewRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +18,23 @@ import lombok.extern.log4j.Log4j2;
 @Transactional
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+
+    // 등록처리
+    public ReviewDTO register(ReviewDTO reviewDTO) {
+        log.info("review register.........");
+
+        try{
+            ReviewEntity reviewEntity = reviewDTO.toEntity();
+
+            reviewRepository.save(reviewEntity);
+
+            return new ReviewDTO(reviewEntity);
+        }catch(DataIntegrityViolationException e){
+            // 외래키 위반
+            throw ReviewException.REVIEW_PRODUCT_NOT_FOUND.get();
+        }catch(Exception e){
+            log.error(e.getMessage());
+            throw ReviewException.REVIEW_NOT_REGISTERED.get();
+        }
+    }
 }
