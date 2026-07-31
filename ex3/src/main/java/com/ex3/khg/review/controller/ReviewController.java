039d13cb463db0db626dbdb2619b8d105ee7c5f8
khg <winquake3@hanmail.net>
2026-07-31 23:06:19 +0900

    리뷰 등록 처리 코드 추가

--- ex3/src/main/java/com/ex3/khg/review/controller/ReviewController.java
+++ ex3/src/main/java/com/ex3/khg/review/controller/ReviewController.java
@@ -1,8 +1,16 @@
 package com.ex3.khg.review.controller;
 
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ex3.khg.review.dto.ReviewDTO;
+import com.ex3.khg.review.exception.ReviewException;
 import com.ex3.khg.review.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +22,14 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     private final ReviewService reviewService;
+
+    // 리뷰 등록 처리
+    @PostMapping("")
+    public ResponseEntity<ReviewDTO> register(@RequestBody @Validated ReviewDTO reviewDTO, Principal principal) {
+        log.info("register: " + reviewDTO);
+        if(!principal.getName().equals(reviewDTO.getReviewer())) {
+            throw ReviewException.REVIEW_MISMATCH.get();
+        }
+        return ResponseEntity.ok(reviewService.register(reviewDTO));
+    }
 }
