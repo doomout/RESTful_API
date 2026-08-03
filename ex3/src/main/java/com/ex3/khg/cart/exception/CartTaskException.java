d5fb76fd40a985d17cfe23b9d6ac633ce98431d4
khg <winquake3@hanmail.net>
2026-08-04 02:49:20 +0900

    카트 예외 메시지 추가

---
+++ ex3/src/main/java/com/ex3/khg/cart/exception/CartTaskException.java
@@ -0,0 +1,39 @@
+package com.ex3.khg.cart.exception;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class CartTaskException {
+    private String message;
+    private int status;
+
+    public CartTaskException(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public static enum Items {
+        NOT_FOUND_CARTITEM("장바구니 상품이 존재하지 않습니다.", 404),
+        NOT_FOUND_CART("장바구니가 존재하지 않습니다.", 404),
+        NOT_FOUND_PRODUCT("상품이 존재하지 않습니다.", 404),
+        INVALID_QUANTITY("유효하지 않은 수량입니다.", 400),
+        DUPLICATE_PRODUCT("이미 장바구니에 존재하는 상품입니다.", 400),
+        CART_ITEM_REGISTER_FAIL("장바구니 상품 등록에 실패했습니다.", 500),
+        CART_ITEM_UPDATE_FAIL("장바구니 상품 수량 변경에 실패했습니다.", 500),
+        CART_ITEM_DELETE_FAIL("장바구니 상품 삭제에 실패했습니다.", 500);
+
+        private String message;
+        private int status;
+
+        Items(String message, int status) {
+            this.message = message;
+            this.status = status;
+        }
+
+        public CartTaskException toException() {
+            return new CartTaskException(this.message, this.status);
+        }
+    }
+}
