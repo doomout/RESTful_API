f998ed8661571000a8d41c01ad8ac36e21925834
khg <winquake3@hanmail.net>
2026-08-04 02:18:46 +0900

    카트 검색 인터페이스 추가

--- ex3/src/main/java/com/ex3/khg/cart/repository/CartRepository.java
+++ ex3/src/main/java/com/ex3/khg/cart/repository/CartRepository.java
@@ -1,9 +1,12 @@
 package com.ex3.khg.cart.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ex3.khg.cart.entity.CartEntity;
 
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
-    
+    // 소유주로 CartEntity를 조회하는 메서드
+    Optional<CartEntity> findByHolder(String holder);
 }
