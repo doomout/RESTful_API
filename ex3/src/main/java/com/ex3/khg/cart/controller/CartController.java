66f2ce78c165af38fbbb5b764913416ad94a9b5e
khg <winquake3@hanmail.net>
2026-08-16 19:22:50 +0900

    장바구니 아이템의 조회

--- ex3/src/main/java/com/ex3/khg/cart/controller/CartController.java
+++ ex3/src/main/java/com/ex3/khg/cart/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.ex3.khg.cart.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -33,6 +38,17 @@ public class CartController {
         List<CartItemDTO> cartItems = cartService.getAllItems(addCartItemDTO.getHolder());
 
         return ResponseEntity.ok(cartItems);
+    }
+    // 장바구니 아이템의 조회
+    @GetMapping("/{cno}")
+    public ResponseEntity<List<CartItemDTO>> getCartItemList(@PathVariable("cno") Long cno, Principal principal) {
+        log.info("get cart..............." + cno);
+        String mid = principal.getName(); // 현재 로그인한 사용자의 mid 가져오기
+        cartService.checkCartHolder(mid, cno);
+        
+        List<CartItemDTO> cartList = cartService.getAllItems(mid);
+        
+        return ResponseEntity.ok(cartList);
     }
     
 }
